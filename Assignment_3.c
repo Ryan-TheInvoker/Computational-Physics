3ce6c32b946eb3977c171f28988d84009931d7b1
Ryan Shearer <24695394@sun.ac.za>
2023-09-18 12:40:48 +0200

    Repo clean up

--- Assignment_3.c
+++ Assignment_3.c
@@ -1,113 +1,10 @@
 
-//TODO: should I be the PRNG once or for each method in my code
-
+//TODO: calculate RMS Error for all cases except the last
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-
-
-typedef struct {
-    int x;
-    int y;
-} Position;
-
-
-//helper methods
-
-//Here we simply just removing walks that intersect themselves
-
-
-// void SAWs_Rejection_Sampling(){
-
-    
-
-//     FILE *output_file = fopen("saw_data.txt", "w");
-//     if (output_file == NULL) {
-//         printf("Error opening file for writing.\n");
-//         exit(0);
-//     }
-
-//     fprintf(output_file, "N, R_N, R_N^2, Fraction\n");  // header for the CSV file
-
-//     int max_N = 1000;  // Maximum number of steps
-//     int step_size = 100;  // Increment step for N
-//     int trials = 1000;  // Number of random walk trials
-
-//     for (int N = step_size; N <= max_N; N += step_size) {
-//         double totalR = 0;
-//         double totalR2 = 0;
-//         int valid_walks = 0;
-//         double averageRN = 0;
-//         double averageRN2 = 0;
-
-
-//         for (int t = 0; t < trials; t++) {
-
-//             Position visited[N];
-//             visited[0] = (Position){0, 0};
-//             Position current_pos = {0, 0};
-
-//             int is_valid = 1;//flag
-
-            
-//             for (int i = 1; i < N; i++) {
-//                 Position last_pos = current_pos;
-//                 int direction = random() % 4;
-//                 switch (direction) {
-                
-//                 case 0: current_pos.x++; break;
-
-//                 case 1: current_pos.x--; break;
-
-//                 case 2: current_pos.y++; break;
-
-//                 case 3: current_pos.y--; break;
-//             }
-
-//             //remember 0 is false in C 
-
-//             // Check if the new position is already visited
-//                 if (isVisited(current_pos, visited, i)) { //
-//                     is_valid = 0;//is not valid
-
-//                     break;  // Exit the loop early if the walk is not valid
-//                 } else {
-//                     visited[i] = current_pos;
-//                     valid_walks++;
-//                 }
-            
-//             }
-
-//                 //valid_walks++ moved into the if
-//                 double R = sqrt(current_pos.x * current_pos.x + current_pos.y * current_pos.y);
-//                 totalR += R;
-//                 totalR2 += R * R;
-//             }
-
-//         if (valid_walks == 0) {
-//             averageRN = 0;
-//             averageRN2 = 0;
-//         } else {
-//             averageRN = totalR / valid_walks;
-//             averageRN2 = totalR2 / valid_walks;
-//             }
-
-//         fprintf(output_file, "%d,  %.5f,  %.5f\n", N, averageRN2, sqrt(averageRN));
-
-        
-//     }
-
-        
-        
-
-    
-
-//     fclose(output_file);
-//     printf("SAW data saved to saw_data.txt\n");
-
-// }
 
 
 
@@ -133,6 +30,7 @@ void SRW() {
 
     for (int N = increment_size; N <= max_N; N += increment_size) {
         double totalRN2 = 0;
+        double totalRN = 0;
 
         for (int t = 0; t < trials; t++) {
             int pos_x = 0, pos_y = 0;  // Start at the origin (x=0, y=0)
@@ -149,10 +47,13 @@ void SRW() {
 
             double RN2 = pos_x * pos_x + pos_y * pos_y; // leave out the square root
             totalRN2 += RN2;
+            totalRN += sqrt(RN2);
         }
 
         double averageRN2 = totalRN2 / trials;
-        fprintf(output_file, "%d,  %.5f,  %.5f\n", N, averageRN2, sqrt(averageRN2));
+        double averageRN = totalRN / trials;
+
+        fprintf(output_file, "%d,  %.8f,  %.8f\n", N, averageRN2, averageRN);
 
     }
 
@@ -164,7 +65,7 @@ void SRW() {
 //SAW_Rejection Sampling -- 2d Array implementation
 #define GRID_SIZE 100
 
-
+ // 2d case for non-reversing walks
 void SAW_rejection() {
      // Seed random number generator
 
@@ -175,17 +76,18 @@ void SAW_rejection() {
         exit(0);
     }
 
-    fprintf(output_file, "N, R_N^2, RN, Fraction\n");  // header for the CSV file
+    fprintf(output_file, "N, R_N^2, RN, CN_estimate\n");  // header for the CSV file
 
-    int max_N = 30;  // Maximum number of steps
+    int max_N = 26;  // Maximum number of steps
     int increment_size = 1;  // Increment step for N
-    int trials = 500000;
+    int trials = 50000;
 
     int direction=0;
     int temp;
     for (int N = 1; N <= max_N; N+= increment_size) {
-        double totalRN2 = 0.0;
-        double validWalkCount = 0;
+        double totalRN2 = 0;
+        double totalRN = 0;
+        double validWalkCount = 0.0;
 
         for (int t = 0; t < trials; t++) {
             int grid[GRID_SIZE][GRID_SIZE] = {0};  // Initialize grid with zeros
@@ -203,9 +105,9 @@ void SAW_rejection() {
                 //This part of the function ensures that the walk is non-reversing.
                 // Random number between 0 and 3
                 int generate = (int)(drand48() * 4);
+
                 if(i>0){
 
-                    
                     while(generate == temp){
                         generate = (int)(drand48() * 4);
                     }
@@ -221,8 +123,8 @@ void SAW_rejection() {
                 }
                 
                 temp = direction;
-                // Check if out of bounds or revisiting a site
-                if ( grid[x][y] == 1) {    // optional grid size implementation: x < 0 || x >= GRID_SIZE || y < 0 || y >= GRID_SIZE ||
+                // Check if  revisiting a site
+                if ( grid[x][y] == 1) {   
                     validWalk = 0;
                     break;
                 }
@@ -236,13 +138,22 @@ void SAW_rejection() {
                 int yDistance = y-yStart;
                 double RN2 = xDistance * xDistance + yDistance * yDistance;  // No need for square root
                 totalRN2 += RN2;
+                totalRN += sqrt(RN2);
                 validWalkCount++;
             }
             
         }
+        
+       
+        double averageRN2 = totalRN2 /validWalkCount; 
+        double averageRN = totalRN/validWalkCount;
+        long double dimensions = 2.0L;
+        
+        long double total_NRW = (2.0L * dimensions) * powl((2.0L * dimensions - 1.0L), (long double)(N - 1));
+        double fraction = validWalkCount/trials;
+        long double result = fraction * total_NRW;
 
-        double averageRN2 = totalRN2 / validWalkCount; //TODO: replaced trials with validWalkCount
-        fprintf(output_file, "%d, %.4f, %.4f, %.10f\n", N, averageRN2, sqrt(averageRN2),validWalkCount/trials);
+        fprintf(output_file, "%d, %.10f, %.10f, %.10Lf\n", N, averageRN2, averageRN, result);
     }
 
     fclose(output_file);
@@ -250,13 +161,277 @@ void SAW_rejection() {
 }
 
 
-//myopic_sampling: in this first rendition we are chooosing the next move from the unvisited sites with equal probability.
+//myopic_sampling: in this first rendition we are choosing the next move from the unvisited sites with equal probability.
+
+// Myopic SAW function
+void SAW_myopic() {
+   // Initialization (Replace these with your actual initializations)
+    int max_N = 100;
+    int trials = 500000;
+    
+    FILE *output_file = fopen("SAW_myopic_data.txt", "w");
+
+    if (output_file == NULL) {
+        printf("Error opening file for writing.\n");
+        exit(1);
+    }
+
+    fprintf(output_file, "N, Average_RN2,Average_RN\n");
+
+    for (int N = 1; N <= max_N; N++) {
+        double totalRN2 = 0.0;
+        int validWalks = 0;
+        double totalRN = 0;
+        for (int t = 0; t < trials; t++) {
+            int grid[GRID_SIZE][GRID_SIZE] = {0};
+            int x = GRID_SIZE / 2, y = GRID_SIZE / 2;
+            int xStart = x, yStart = y;
+            
+            grid[x][y] = 1;
+            int validWalk = 1;
+
+            for (int i = 0; i < N; i++) {
+
+                int neighbors[4][2] = {0};
+                int count = 0;
+
+                // Check unvisited neighbors
+                if (grid[x-1][y] == 0) {
+                    neighbors[count][0] = x-1; neighbors[count][1] = y; count++;
+                }
+                if (grid[x+1][y] == 0) {
+                    neighbors[count][0] = x+1; neighbors[count][1] = y; count++;
+                }
+                if (grid[x][y-1] == 0) {
+                    neighbors[count][0] = x; neighbors[count][1] = y-1; count++;
+                }
+                if ( grid[x][y+1] == 0) {
+                    neighbors[count][0] = x; neighbors[count][1] = y+1; count++;
+                }
+
+                //if count remains zero it means that the walker is stuck
+                if (count == 0) {
+                    validWalk = 0;
+                    break;
+                }
+
+                // Choose a random unvisited neighbor
+                int choice = (int)(drand48() * count);
+                x = neighbors[choice][0];
+                y = neighbors[choice][1];
+                grid[x][y] = 1;
+            }
+
+            if (validWalk) {
+                validWalks++;
+                int xDistance = x-xStart;
+                int yDistance = y-yStart;
+                double RN2 = xDistance * xDistance + yDistance * yDistance;
+                
+                totalRN2 += RN2;
+                totalRN += sqrt(RN2);
+            }
+        }
+
+        double averageRN2 = (validWalks == 0) ? 0 : totalRN2 / validWalks;
+        double averageRN = (validWalks == 0) ? 0 : totalRN / validWalks;
+        fprintf(output_file, "%d, %.8f, %.8f\n", N, averageRN2,averageRN);   
+    }
+
+    fclose(output_file);
+    printf("Data saved to saw_myopic_data.txt\n");
+}
+
+
+
+//In this rendition we are using self-importance sampling to try and berid ourselves of any pesky bias
+void SAW_myopic_Rosenbluth(int max_N, int trials) {
+
+    FILE *output_file = fopen("saw_rosenbluth_data.txt", "w");
+    if (output_file == NULL) {
+        printf("Error opening file for writing.\n");
+        exit(1);
+    }
+    fprintf(output_file, "N, R_N^2, RN\n");
+
+    for (int N = 1; N <= max_N; N++) {
+        double weightedSumRN2 = 0.0;
+        double weightedSumRN = 0.0;
+        double totalWeight = 0.0;
+
+        for (int t = 0; t < trials; t++) {
+            int grid[GRID_SIZE][GRID_SIZE] = {0};
+            int x = GRID_SIZE / 2, y = GRID_SIZE / 2;
+            int xStart = x, yStart = y;
+            double W = 1.0;  // Initialize weight
+            grid[x][y] = 1;
+
+            for (int i = 0; i < N; i++) {
+               
+                int neighbors[4][2] = {0};
+                int count = 0;
+
+                if (grid[x-1][y] == 0) {
+                    neighbors[count][0] = x-1; neighbors[count][1] = y; count++;
+                }
+                if (grid[x+1][y] == 0) {
+                    neighbors[count][0] = x+1; neighbors[count][1] = y; count++;
+                }
+                if (grid[x][y-1] == 0) {
+                    neighbors[count][0] = x; neighbors[count][1] = y-1; count++;
+                }
+                if ( grid[x][y+1] == 0) {
+                    neighbors[count][0] = x; neighbors[count][1] = y+1; count++;
+                }
+
+                if (count == 0) {
+                    W = 0;
+                    break;
+                }
+
+                W *= count;  // Update weight
+
+     
+                
+
+                // Choose a random unvisited neighbor
+                int choice = (int)(drand48() * count);
+                x = neighbors[choice][0];
+                y = neighbors[choice][1];
+
+
+                grid[x][y] = 1;
+            }
+            int xDistance = x-xStart, yDistance = y-yStart;
+            double RN2 = xDistance * xDistance + yDistance * yDistance;
+            double RN = sqrt(RN2);
+            weightedSumRN2 += W * RN2;
+            weightedSumRN += W * RN;
+
+
+            totalWeight += W;
+        }
+
+        double averageRN2 = (totalWeight == 0) ? 0 : weightedSumRN2 / totalWeight;
+        double averageRN = (totalWeight == 0) ? 0 : weightedSumRN / totalWeight;
+
+        fprintf(output_file, "%d, %.10f, %.10f\n", N, averageRN2, averageRN);
+    }
+
+    fclose(output_file);
+    printf("Data saved to saw_rosenbluth_data.txt\n");
+
+}
+
+
+//3D case
+void SAW_rejection_3D() {
+     
+
+    FILE *output_file = fopen("saw_3D_data.txt", "w");
+
+    if (output_file == NULL) {
+        printf("Error opening file for writing.\n");
+        exit(0);
+    }
+
+    fprintf(output_file, "N, R_N^2, RN, Fraction\n");  // header for the CSV file
+
+    int max_N = 40;  // Maximum number of steps
+    int increment_size = 1;  // Increment step for N
+    int trials = 50000;
+
+    int direction=0;
+    int temp;
+    for (int N = 1; N <= max_N; N+= increment_size) {
+        double totalRN2 = 0;
+        double totalRN = 0;
+        double validWalkCount = 0;
+
+        for (int t = 0; t < trials; t++) {
+            int grid[GRID_SIZE][GRID_SIZE][GRID_SIZE] = {0};  // Initialize grid with zeros
+            int x = GRID_SIZE / 2, y = GRID_SIZE / 2, z = GRID_SIZE / 2;  // Start at the center
+            grid[x][y][z] = 1;  // Mark the starting point as visited
+            
+            int validWalk = 1;  // Flag to check if the walk is valid
+            
+            //starting positions of x and y
+            int xStart = x;
+            int yStart = y;
+            int zStart = z;
+
+            for (int i = 0; i < N; i++) {
+                
+                //This part of the function ensures that the walk is non-reversing.
+                // Random number between 0 and 5
+                int generate = (int)(drand48() * 6);
+
+                //if just to skip for first case
+                if(i>0){
+
+                    while(generate == temp){
+                        generate = (int)(drand48() * 6);
+                    }
+                }
+
+                direction = generate;
+                  
+                switch (direction) {
+                    case 0: x++; break;  
+                    case 1: x--; break;  
+                    case 2: y++; break;  
+                    case 3: y--; break;  
+                    case 4: z++; break;
+                    case 5: z--; break;
+                }
+                
+                temp = direction;
+                // Check if out of bounds or revisiting a site
+                if ( grid[x][y][z] == 1) {    
+                    validWalk = 0;
+                    break;
+                }
+
+                grid[x][y][z] = 1;  // Mark the new site as visited
+            }
+
+            if (validWalk) {
+                
+                int xDistance = x - xStart;
+                int yDistance = y - yStart;
+                int zDistance = z - zStart;
+                double RN2 = xDistance * xDistance + yDistance * yDistance + zDistance * zDistance; 
+                totalRN2 += RN2;
+                totalRN += sqrt(RN2);
+
+                validWalkCount++;
+            }
+            
+        }
+
+        double averageRN2 = totalRN2 / validWalkCount; 
+        double averageRN = totalRN / validWalkCount;
+        int dimensions = 3;
+        int total_NRW = (2*dimensions)*pow((2*dimensions-1),(N-1));
+
+        fprintf(output_file, "%d, %.10f, %.10f, %.10f\n", N, averageRN2, averageRN,(validWalkCount/(double)trials)*total_NRW);
+    }
+
+    fclose(output_file);
+    printf("Data saved to saw_3D_data.txt\n");
+}
+
+
+
 
 int main(){
     srand48(time(0));  // Seed random number generator
-    srandom(time(NULL)); 
+     
     SRW();
     SAW_rejection();
+    SAW_myopic();
+    SAW_myopic_Rosenbluth(100,500000);
+    SAW_rejection_3D();
 
     printf("I Ran through Rand al' Thor");
     return 0;
